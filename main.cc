1cf4de0bb5939e6701d35892a3429489bd176e5f
Gustav <sir.gustav.the.coder@gmail.com>
2020-08-10 11:11:40 +0200

    create a opengl window

--- main.cc
+++ main.cc
@@ -1,12 +1,44 @@
+#include "glad/glad.h"
 #include "SDL.h"
+
 
 int
 main(int, char**)
 {
-    // int width = 1280;
-    // int height = 720;
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
 
-    bool running = false;
+    int width = 1280;
+    int height = 720;
+
+    SDL_Window* window = SDL_CreateWindow
+    (
+        "TRED",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        width,
+        height,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    if(window == nullptr)
+    {
+        SDL_Log("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
+        SDL_Log("Failed to initialize OpenGL context");
+        return -1;
+    }
+
+    bool running = true;
 
     while(running)
     {
@@ -15,6 +47,12 @@ main(int, char**)
         {
             switch(e.type)
             {
+            case SDL_WINDOWEVENT:
+                if(e.window.event == SDL_WINDOWEVENT_RESIZED)
+                {
+                    // todo
+                }
+                break;
             case SDL_QUIT:
                 running = false;
                 break;
@@ -44,11 +82,17 @@ main(int, char**)
             }
         }
 
-        // ClearScreen(Color::Black);
+        glClearColor(0,0,0,1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
         // world.Render(viewport_handler.GetFullViewport(), camera);
-        // SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(window);
     }
 
+    SDL_GL_DeleteContext(glcontext);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
     return 0;
 }
 
