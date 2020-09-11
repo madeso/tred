1ebfac37493b5c00dc539158a67617a7a59f422c
Gustav <sir.gustav.the.coder@gmail.com>
2020-09-11 21:37:21 +0200

    wrote down some design notes

---
+++ design.cc
@@ -0,0 +1,69 @@
+// design or mockup of engine api... this is not compiled nor part of tred code but might be soon...
+
+// std things?
+struct string {};
+template <typename T> struct vector {};
+template <typename K, typename V> struct map {};
+
+// math things
+struct vec2f; struct vec3f; struct ray3f;
+struct rect; struct aabb;
+
+// engine things
+
+struct Pose
+{
+};
+
+struct Animation
+{
+    float length;
+    Pose GetPose(float time) const;
+};
+
+struct ActorDef
+{
+    map<string, Animation> animations;
+    // definition of a mesh
+};
+
+struct Actor
+{
+    // position
+    // rotation
+    // current animation pose
+    // ActorDef
+
+    vec3f WorldToObjectSpace(vec3f) const;
+    vec3f ObjectToWorldSpace(vec3f) const;
+};
+
+// frustum(perspective) or a box(ortho)
+struct ViewVolume
+{
+    // contains the 6 planes
+    // function to test if aabb, sphere, etc are inside
+};
+
+struct Camera
+{
+    // virtual width & height
+    // world position
+    // near and far plane
+
+    // move to the compiled camera or a util function that compiles executes and discards? or dont bother compiling?
+    ViewVolume GetViewVolume();
+    ray3f CameraToRay(vec2f) const;
+    vec2f WorldToCameraSpace(vec3f) const;
+    rect WorldToCameraSpace(aabb) const;
+};
+
+struct World
+{
+    void AddActor(Actor);
+};
+void Render(const World& world, const Camera& camera);
+
+// fullscreen effects are handled elsewhere, not sure yet how to approach this
+
+// todo(Gustav): figure out how to handle figure out how to handle 2d rendering both immediate modeish and tiled/world rendering
