789a2c3e1bc482809d15fe7689d4ae16e30a614a
Gustav <sir.gustav.the.coder@gmail.com>
2020-12-20 13:27:37 +0100

    changes that works with master imgui

--- tools/embed_binary.cpp
+++ tools/embed_binary.cpp
@@ -1,6 +1,7 @@
 // imgui/misc/fonts
-#define CUSTOM_BINARY_TO_COMPRESSED_C custom_binary_to_compressed_c
+#define main main_not_used
 #include "binary_to_compressed_c.cpp"
+#undef main
 
 bool custom_binary_to_compressed_c(const char* filename, const char* symbol, bool use_base85_encoding, bool use_compression)
 {
@@ -34,3 +35,35 @@ bool custom_binary_to_compressed_c(const char* filename, const char* symbol, boo
 
     return result;
 }
+
+
+// copied from binary_to_compressed_c.cpp but changed call from binary_to_compressed_c to custom_binary_to_compressed_c
+
+int main(int argc, char** argv)
+{
+    if (argc < 3)
+    {
+        printf("Syntax: %s [-base85] [-nocompress] <inputfile> <symbolname>\n", argv[0]);
+        return 0;
+    }
+
+    int argn = 1;
+    bool use_base85_encoding = false;
+    bool use_compression = true;
+    if (argv[argn][0] == '-')
+    {
+        if (strcmp(argv[argn], "-base85") == 0) { use_base85_encoding = true; argn++; }
+        else if (strcmp(argv[argn], "-nocompress") == 0) { use_compression = false; argn++; }
+        else
+        {
+            fprintf(stderr, "Unknown argument: '%s'\n", argv[argn]);
+            return 1;
+        }
+    }
+
+    bool ret = custom_binary_to_compressed_c(argv[argn], argv[argn + 1], use_base85_encoding, use_compression);
+    if (!ret)
+        fprintf(stderr, "Error opening or reading file: '%s'\n", argv[argn]);
+    return ret ? 0 : 1;
+}
+
