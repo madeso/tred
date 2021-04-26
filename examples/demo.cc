d7c7e3b6426284eafb7f4ee8f409391166621ab0
Gustav <sir.gustav.the.coder@gmail.com>
2021-04-26 22:21:57 +0200

    rendering g

--- examples/demo.cc
+++ examples/demo.cc
@@ -1,11 +1,13 @@
 #include "tred/fyro.h"
 
 #include "cards.png.h"
+#include "letter_g.png.h"
 #include "sprites/cards.h"
 
 struct ExampleGame : public Game
 {
     Texture cards;
+    Texture letter_g;
 
     ExampleGame()
         : cards
@@ -13,6 +15,16 @@ struct ExampleGame : public Game
             LoadImageEmbeded
             (
                 CARDS_PNG,
+                TextureEdge::Clamp,
+                TextureRenderStyle::Pixel,
+                Transperency::Include
+            )
+        )
+        , letter_g
+        (
+            LoadImageEmbeded
+            (
+                LETTER_G_PNG,
                 TextureEdge::Clamp,
                 TextureRenderStyle::Pixel,
                 Transperency::Include
@@ -27,16 +39,14 @@ struct ExampleGame : public Game
         // auto r = with_layer_fit_with_bars(rc, 200.0f, 200.0f, glm::mat4(1.0f));
         auto r = with_layer_extended(rc, 200.0f, 200.0f, glm::mat4(1.0f));
 
-        // todo(Gustav): figure out uv layout... what is up/down? what is left/right
-        // what do we want? like euph? probably...
-        // render the G sprite to test orientation
-
         r.batch->quad({}, r.viewport_aabb_in_worldspace, {}, {0.8, 0.8, 0.8, 1.0f});
 
         constexpr auto card_sprite = Cint_to_float(::cards::back).zero().set_height(30);
 
         r.batch->quad(&cards, card_sprite.translate(100, 100), ::cards::hearts[2]);
         r.batch->quad(&cards, card_sprite.translate(10, 50), ::cards::back);
+
+        r.batch->quad(&letter_g, rect{40, 40}.translate(50, 50), {});
     }
 };
 
