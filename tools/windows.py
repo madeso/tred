92bdd2f3a5ddeb9e037e47f9cef25ea3b389ff71
Gustav <sir.gustav.the.coder@gmail.com>
2021-10-30 00:22:15 +0200

    ci: started on ci

---
+++ tools/windows.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""build script for windows for ride"""
+
+import buildtools.windows as build
+
+
+def main():
+    """main function"""
+    data = build.default_data('tred')
+    build.add_dependency_sdl2(data)
+    build.add_dependency_python(data)
+    build.main(data)
+
+
+if __name__ == "__main__":
+    main()
