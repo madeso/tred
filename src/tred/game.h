bef8d125c55b18cd3ec3b5a8e21107654966e3cc
Gustav <sir.gustav.the.coder@gmail.com>
2021-11-06 11:20:54 +0100

    refactor: moved layer2 to own file

--- src/tred/game.h
+++ src/tred/game.h
@@ -15,48 +15,7 @@
 #include "tred/spritebatch.h"
 #include "tred/input/key.h"
 #include "tred/render2.h"
-
-
-struct Command2
-{
-    glm::ivec2 size;
-};
-
-struct RenderCommand2
-{
-    Render2* render;
-    glm::ivec2 size;
-};
-
-struct Layer2
-{
-    Rectf viewport_aabb_in_worldspace;
-    Recti screen;
-
-    glm::vec2 mouse_to_world(const glm::vec2& p) const;
-};
-
-struct RenderLayer2 : Layer2
-{
-    SpriteBatch* batch;
-
-    RenderLayer2(Layer2&& l, SpriteBatch* batch);
-
-    ~RenderLayer2();
-};
-
-enum class ViewportStyle { black_bars, extended};
-
-struct LayoutData
-{
-    ViewportStyle style;
-    float requested_width;
-    float requested_height;
-    const glm::mat4 camera;
-};
-
-RenderLayer2 with_layer(const RenderCommand2& rc, const LayoutData& ld);
-Layer2 with_layer(const Command2& rc, const LayoutData& ld);
+#include "tred/layer2.h"
 
 struct Game
 {
